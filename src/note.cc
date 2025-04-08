0955136241c7eb5b07181671d23f50c5ef6461f7
Matt Wang <matthewianwang@gmail.com>
2025-04-07 19:09:44 -0700

    Implemented load and save file functions

--- src/note.cc
+++ src/note.cc
@@ -1,9 +1,44 @@
 #include "note.h"
 
-static Note LoadFromFile(const std::filesystem::path& path) {
-    return {};
+#include <filesystem>
+#include <fstream>
+
+
+Note Note::LoadFromFile(const std::filesystem::path& path) {
+    Note note;
+    note.filename = path.filename().string();
+    std::ifstream in(path);
+
+    if (!in) {
+        return note;
+    }
+
+    std::string line;
+    bool first_line = true;
+    while(std::getline(in, line)) {
+        if (first_line) {
+            if (line.rfind("# ", 0) == 0) {
+                note.title = line.substr(2);
+            } else {
+                note.title = line;
+            }
+            first_line = false;
+        } else {
+            note.content += line + "\n";
+        }
+    }
+
+    return note;
 }
 
-void SaveToFile(const std::filesystem::path& path) {
-    
+void Note::SaveToFile(const std::filesystem::path& path) const {
+    std::filesystem::path final_path = path / filename;
+    std::ofstream out(final_path);
+
+    if (!out) {
+        return;
+    }
+
+    out << "# " << title << std::endl;
+    out << content;
 }
