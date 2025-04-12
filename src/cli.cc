342f1da7d8b0803f5db5be5aade92660a3a320e8
Matt Wang <matthewianwang@gmail.com>
2025-04-11 23:53:07 -0700

    Implemented JSON parsing to get note directory

--- src/cli.cc
+++ src/cli.cc
@@ -2,6 +2,7 @@
 #include "index.h"
 #include "utils/display_utils.h"
 #include "utils/input_validation_utils.h"
+#include "utils/config_utils.h"
 
 #include <iostream>
 #include <memory>
@@ -77,7 +78,11 @@ void DispatchCommand(const std::vector<std::string>& command_args) {
         return;
     }
 
-    std::string notes_directory = "notes";
+    std::string notes_directory = config_utils::LoadNotesDirectory(std::filesystem::current_path());
+    if (notes_directory.empty()) {
+        std::cout << "Missing or invalid .notedb/config.json in current directory.\n";
+        return;
+    }
 
     std::shared_ptr<SearchIndex> index = std::make_shared<SearchIndex>();
     index->BuildIndex(notes_directory);
--- src/utils/config_utils.cc
+++ src/utils/config_utils.cc
@@ -1,8 +1,37 @@
 #include "utils/config_utils.h"
+
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 #include <string>
+
 namespace config_utils {
+
 std::string LoadNotesDirectory(const std::filesystem::path& root_directory) {
+    std::ifstream in(root_directory / ".notedb" / "config.json");
+    if (!in) {
+        return "";
+    }
+
+    std::string line;
+    while (std::getline(in, line)) {
+        std::size_t key = line.find("\"root\"");
+
+        if (key != std::string::npos) {
+            std::size_t colon = line.find(':', key);
+            if (colon == std::string::npos) {
+                continue;
+            }
+
+            std::size_t start = line.find('"', colon);
+            std::size_t end = line.find('"', start + 1);
+
+            if (start != std::string::npos && end != std::string::npos) {
+                return line.substr(start + 1, end - start - 1);
+            }
+        }
+    }
+
     return "";
 }
 
