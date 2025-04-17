3af2b72912d8aa8798d28332fe90e23a124df33d
Matt Wang <matthewianwang@gmail.com>
2025-04-16 20:11:02 -0700

    Added tests for add/edit/delete CLI commands

--- tests/cli_test.cc
+++ tests/cli_test.cc
@@ -1,8 +1,11 @@
-#include <gtest/gtest.h>
+#include <chrono>
 #include <filesystem>
+#include <gtest/gtest.h>
 #include <string>
+#include <thread>
 
 #include "cli.h"
+#include "note.h"
 #include "utils/config_utils.h"
 
 class CliTest : public ::testing::Test {
@@ -27,3 +30,43 @@ void CliTest::TearDown() {
     config_utils::SetupConfigFile({saved_notes_directory_}, {});
 }
 
+TEST_F(CliTest, NewCommandCreatesFile) {
+    std::string filename = "test_create.md";
+    std::filesystem::path expected_path = test_directory_ / filename;
+
+    ASSERT_FALSE(std::filesystem::exists(expected_path));
+
+    cli::DispatchCommand({"new", filename, "--editor", "none"});
+
+    EXPECT_TRUE(std::filesystem::exists(expected_path));
+}
+
+TEST_F(CliTest, EditCommandUpdatesFile) {
+    std::string filename = "test_edit.md";
+    std::filesystem::path expected_path = test_directory_ / filename;
+
+    cli::DispatchCommand({"new", filename, "--editor", "none"});
+
+    ASSERT_TRUE(std::filesystem::exists(expected_path));
+
+    auto original_write_time = std::filesystem::last_write_time(expected_path);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    cli::DispatchCommand({"edit", filename, "--editor", "none"});
+    auto new_write_time = std::filesystem::last_write_time(expected_path);
+
+    EXPECT_GT(new_write_time, original_write_time);
+}
+
+TEST_F(CliTest, DeleteCommandRemovesFile) {
+    std::string filename = "test_delete.md";
+    std::filesystem::path expected_path = test_directory_ / filename;
+
+    cli::DispatchCommand({"new", filename, "--editor", "none"});
+
+    ASSERT_TRUE(std::filesystem::exists(expected_path));
+
+    cli::DispatchCommand({"delete", filename});
+
+    EXPECT_FALSE(std::filesystem::exists(expected_path));
+}
